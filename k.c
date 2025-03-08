a36916d3f273556192c45108a18d7fde3fd0abe5
antoineafonin <antoineafonin@gmail.com>
2025-03-08 16:17:09 -0500

    function is move possible

--- k.c
+++ k.c
@@ -29,3 +29,35 @@ bool is_game_won(const struct game game) {
 
     return false;
 }
+
+bool is_move_possible(const struct game game) {
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            char letter = game.board[row][col];
+
+            if (row % 2 != col % 2) continue;
+
+            // check bottom
+            if (letter == game.board[row + 1][col] && row < SIZE - 1) {
+                return true;
+            }
+
+            // check top
+            if (letter == game.board[row - 1][col] && row >= 1) {
+                return true;
+            }
+
+            // check left to right
+            if (letter == game.board[row][col + 1] && col < SIZE - 1) {
+                return true;
+            }
+
+            // check right to left
+            if (letter == game.board[row][col - 1] && col >= 1) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
--- main.c
+++ main.c
@@ -12,5 +12,27 @@ int main() {
 
     printf("is won: %d\n", is_game_won(game));
 
+    struct game game1 = {.board = {{'A', 'A', 'C', 'D'},
+                                   {'A', 'F', 'G', 'H'},
+                                   {'I', 'J', 'J', 'A'},
+                                   {'B', 'C', 'D', 'E'}},
+                         .score = 0};
+
+    printf("is move possible: %d\n", is_move_possible(game1));
+    // stdout: 1
+
+    struct game gam2 = {
+        .board = {
+            {'A', 'B', 'C', 'D'},
+            {'E', 'F', 'G', 'H'},
+            {'I', 'J', 'K', 'A'},
+            {'B', 'C', 'D', 'E'}
+        },
+        .score = 0
+    };
+    
+    printf("is move possible: %d\n", is_move_possible(gam2));
+    // stdout: 0
+
     return 0;
 }
